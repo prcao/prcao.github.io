64f5836c1311a9fa10b2acf98fc6d29092401e68
prcao <patrickrcao@gmail.com>
2018-07-19 17:33:47 -0400

    add footer and contact

--- js/navbar.js
+++ js/navbar.js
@@ -2,15 +2,30 @@ const NAVBAR_ID = "navbar";
 
 const NAVBAR = document.getElementById(NAVBAR_ID);
 
-window.onscroll = () => {
+window.onscroll = function () {
 
     var scrollThreshold = document.getElementById("landing").offsetHeight;
 
     if (document.body.scrollTop >= scrollThreshold || document.documentElement.scrollTop >= scrollThreshold) {
         NAVBAR.style.top = "0";
-    } 
-    
+    }
+
     else {
         NAVBAR.style.top = "-" + scrollThreshold + "px";
     }
 }
+
+$(document).ready(
+
+    function () {
+
+        // Check for click events on the navbar burger icon
+        $(".navbar-burger").click(function () {
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            $(".navbar-burger").toggleClass("is-active");
+            $(".navbar-menu").toggleClass("is-active");
+
+        });
+    }
+);
